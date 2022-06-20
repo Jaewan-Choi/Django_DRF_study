c4b7894d1876b4b2c06abd1f6cc2e011c52d4179
JaeWan CHOI <wkdudhksl@gamil.com>
2022-06-20 20:54:16 +0900

    6/20 과제

--- blog/views.py
+++ blog/views.py
@@ -1,44 +1,11 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import status
-from rest_framework import permissions
-from rest_framework.permissions import BasePermission
-from rest_framework.exceptions import APIException
+# from rest_framework import permissions
+from DRF.permissions import IsAdminOrIsAuthenticatedReadOnly
 from user.models import User as UserModel
 from .models import Article as ArticleModel
 from .models import Category as CategoryModel
 from datetime import datetime, timedelta
-
-
-class GenericAPIException(APIException):
-    def __init__(self, status_code, detail=None, code=None):
-        self.status_code=status_code
-        super().__init__(detail=detail, code=code)
-
-
-class IsAdminOrIsAuthenticatedReadOnly(BasePermission):
-    SAFE_METHODS = ('GET', )
-    message = '접근 권한이 없습니다.'
-
-    def has_permission(self, request, view):
-        user = request.user
-
-        if not user.is_authenticated:
-            response ={
-                    "detail": "서비스를 이용하기 위해 로그인 해주세요.",
-                }
-            raise GenericAPIException(status_code=status.HTTP_401_UNAUTHORIZED, detail=response)
-
-        if user.is_authenticated and user.join_date < (datetime.now() - timedelta(days=7)):
-            return True
-            
-        elif user.is_admin:
-            return True
-            
-        elif user.is_authenticated and request.method in self.SAFE_METHODS:
-            return True
-        
-        return False
 
 
 class Article(APIView):
